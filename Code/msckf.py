def5609dbfb3b82838157e47c1b45df8681edf46
alexchiluisac <ajchiluisa@wpi.edu>
2022-11-30 11:38:22 -0500

    added feature observation

--- Code/msckf.py
+++ Code/msckf.py
@@ -534,12 +534,27 @@ class MSCKF(object):
         """
         # get the current imu state id and number of current features
         ...
+
+        state_id = self.state_server.imu_state.id
+        curr_feature_num = len(self.map_server)
+        tracked_feature_num = 0
         
         # add all features in the feature_msg to self.map_server
         ...
 
+        for feature in feature_msg.features:
+            if feature.id not in self.map_server:
+                # This is a new feature.
+                map_feature = Feature(feature.id, self.optimization_config)
+                map_feature.observations[state_id] = np.array([feature.u0, feature.v0, feature.u1, feature.v1])
+                self.map_server[feature.id] = map_feature
+            else:
+                # This is an old feature.
+                self.map_server[feature.id].observations[state_id] = np.array([feature.u0, feature.v0, feature.u1, feature.v1])
+                tracked_feature_num += 1
         # update the tracking rate
         ...
+        self.tracking_rate = tracked_feature_num / (curr_feature_num+1e-5)
 
     def measurement_jacobian(self, cam_state_id, feature_id):
         """
